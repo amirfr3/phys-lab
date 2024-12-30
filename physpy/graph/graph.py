fbcb342fea67e9c985c8f946a4e3ba8a94ec61ba
amirfr3 <79229042+amirfr3@users.noreply.github.com>
2024-12-30 22:43:42 +0200

    Update graph.py
    Oops. fixed string

--- physpy/graph/graph.py
+++ physpy/graph/graph.py
@@ -100,7 +100,7 @@ def make_graph(
     if show:
         if debug_show:
             print(
-                f"=== EXAMPLE DATA FOR {graph_title_rtl} ===\n{processed_data["data"][:5]}\n================="
+                f"=== EXAMPLE DATA FOR {graph_title_rtl} ===\n{processed_data['data'][:5]}\n================="
             )
         print(processed_data["fit_results"])
         plt.show()

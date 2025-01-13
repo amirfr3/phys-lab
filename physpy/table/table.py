7820758a2d01ea3e3bac1c89bd2d118d1b0d69b0
Amir F <amirfr@gmail.com>
2025-01-13 22:33:54 +0200

    Added initial thing

---
+++ physpy/table/table.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+def _read_params(params):
+    # Add values to global namespace
+    for name, series in params.items():
+        globals()["P_" + name] = tuple(series.to_numpy()[:2])  # )
+
+
+def parse_data(filepath):
+    fit_tables = []
+    params = None
+    with pd.ExcelFile(filepath) as xl:
+        for sheet in xl.sheet_names:
+            if sheet.endswith('_fit'):
+                fit_tables.append(pd.read_excel(xl, sheet))
+        if sheet.lower() == 'params':
+            params = pd.read_excel(xl, sheet)
+
+    _read_params(params)
+    return fit_tables

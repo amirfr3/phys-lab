fbae798d12a6f70c76166dfd9eb6b16ece6a23b3
Amir F <amirfr@gmail.com>
2025-01-15 01:14:11 +0200

    fixed p_prob

--- physpy/equation/equation.py
+++ physpy/equation/equation.py
@@ -156,7 +156,7 @@ def latexify_and_round_fit_params(fit_data, units=None):
     chi, chi_e = _round_number(fit_data['chi2_red']), _round_number(math.sqrt(2/fit_data['dof']))
     latex_str += _latexify_value('\\chi^2_{red}', chi, chi_e, relative_error=None, units=None) + '\n'
 
-    latex_str += latexify_and_round_value('P_{prob}', fit_data['p_val']) + '\n'
+    latex_str += latexify_and_round_value('P_{prob}', fit_data['p_val'], no_relative_error=True) + '\n'
 
     return latex_str
 

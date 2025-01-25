db8ba4dee31dbff07dd58b7709463e1cd67d4a83
Amir F <amirfr@gmail.com>
2025-01-25 15:45:26 +0200

    chi factor

--- physpy/equation/equation.py
+++ physpy/equation/equation.py
@@ -166,8 +166,8 @@ def latexify_and_round_fit_params(fit_data, units=None):
     for i, (param, error, unit) in enumerate(zip(fit_data['fit_params'], fit_data['fit_params_error'], units)):
         latex_str += latexify_and_round_value(f'a_{i}', param, error, unit=unit) + '\n'
     
-    chi, chi_e = _round_number(fit_data['chi2_red']), _round_number(math.sqrt(2/fit_data['dof']))
-    latex_str += _latexify_value('\\chi^2_{red}', chi, chi_e, relative_error=None, relative_error_factor=None, unit=None) + '\n'
+    chi, chi_e, chi_f = _round_number(fit_data['chi2_red']), _round_number(math.sqrt(2/fit_data['dof']))
+    latex_str += _latexify_value('\\chi^2_{red}', chi, chi_e, chi_f, relative_error=None, relative_error_factor=None, unit=None) + '\n'
 
     latex_str += latexify_and_round_value('P_{prob}', fit_data['p_val'], no_relative_error=True) + '\n'
 

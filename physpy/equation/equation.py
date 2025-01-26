5e564cfc425d81755b5681427482f6d592965db1
Amir F <amirfr@gmail.com>
2025-01-26 22:52:57 +0200

    Oops

--- physpy/equation/equation.py
+++ physpy/equation/equation.py
@@ -131,7 +131,7 @@ def _latexify_value(name, value: float, error, factor, relative_error, relative_
     # Integer prettyfing
     if value.is_integer():
         value = int(value)
-    if error.is_integer():
+    if isinstance(error, float) and error.is_integer():
         error = int(error)
     if relative_error is not None and relative_error.is_integer():
         relative_error = int(relative_error)
